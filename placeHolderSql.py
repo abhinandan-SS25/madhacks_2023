edbfc4933554408465f15ff4dcf91d5808953555
arnav0red <arnav.mohakud@gmail.com>
2023-11-11 16:58:45 -0600

    placeHolderSql pour toi

---
+++ placeHolderSql.py
@@ -0,0 +1,12 @@
+def getRow(username):
+    return {"username":username,"age":5}
+def isUserInListMethod(username):
+    if(username=="John"):
+        return True
+    return False
+def returnMatchedRow(username,password):
+    if(username=="John"):
+        return {"username":username,"age":5}
+    return None
+def usersAtLocation(username, location):
+    return (username,"john","jamie","jack")
