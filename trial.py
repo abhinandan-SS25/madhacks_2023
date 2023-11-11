109a29737ce84e97b6d5bcb885f69710897f558f
arnav0red <arnav.mohakud@gmail.com>
2023-11-11 17:41:03 -0600

    added trial

---
+++ trial.py
@@ -0,0 +1,25 @@
+from madhacks_2023.Database import Database
+
+database = Database()
+exampleDict = {
+    "userName": "test",
+    "phoneNum": "test",
+    "verification": "test",
+    "password": "test",
+    "description": "test",
+    "streetAddress": "test",
+    "city": "test",
+    "state": "test",
+    "country": "test",
+    "pincode": "test",
+    "ownerName": "test",
+    "ownerDOB": "test",
+    "ownerSex": "test",
+    "dogName": "test",
+    "dogBreed": "test",
+    "dogDOB": "test",
+    "dogSex": "test",
+    "dogsFavoriteActivities": "test",
+}
+database.insertUser(exampleDict)
+print(database.getUser("test","test"))
