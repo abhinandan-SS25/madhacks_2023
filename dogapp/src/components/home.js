934df9069618386af2e54cb2215ee1387b89080f
Abhinandan Saha <abhinandan.ss252003@gmail.com>
2023-11-12 08:00:47 -0600

    css for login, register

--- dogapp/src/components/home.js
+++ dogapp/src/components/home.js
@@ -4,7 +4,16 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
       <div className='home'>
-        Home
+        <div className="header">
+          This is Bark Avenue
+        </div>
+        <div className="subheader" >
+          Here you can find dogs nerby and arrange playdates!
+        </div>
+        <div className='lowbar flex_center'>
+            <Link className='bar flex_center' to="/login">Login</Link>
+            <Link className='bar flex_center' to="/register">Register</Link>
+        </div>
       </div>
     );
   }
