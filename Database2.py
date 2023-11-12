ffc80b1980ad2b6ed9b878713ed8d3dd048ece25
V-Agarwal-25 <137364163+VAgarwal46@users.noreply.github.com>
2023-11-11 21:51:33 -0600

    failed testing mongo

--- Database2.py
+++ Database2.py
@@ -1,10 +1,2 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = "mongodb+srv://VAgarwal46:VAgarwal2512@vagarwal46.s1zbw9l.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+import pymongo
+
