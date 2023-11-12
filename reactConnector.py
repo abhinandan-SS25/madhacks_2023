8bf4e10fcc03a1e22c18f076919e66c00650077e
arnav0red <arnav.mohakud@gmail.com>
2023-11-11 19:57:46 -0600

    completed register, login

--- reactConnector.py
+++ reactConnector.py
@@ -2,36 +2,40 @@ from flask import Flask, request, redirect, url_for, jsonify, Response
 from flask_cors import CORS
 import json
 import placeHolderSql
-import Database
+import Database, re
 
 database = Database.Database()
 app = Flask(__name__)
 CORS(app)
 
+
 def createExampleNames():
     exampleDict = {
-    "userName": "test",
-    "phoneNum": "test",
-    "verification": "test",
-    "password": "test",
-    "description": "test",
-    "streetAddress": "test",
-    "city": "test",
-    "state": "test",
-    "country": "test",
-    "pincode": "test",
-    "ownerName": "test",
-    "ownerDOB": "test",
-    "ownerSex": "test",
-    "dogName": "test",
-    "dogBreed": "test",
-    "dogDOB": "test",
-    "dogSex": "test",
-    "dogsFavoriteActivities": "test",
-}
+        "userName": "test",
+        "phoneNum": "test",
+        "verification": "test",
+        "password": "test",
+        "description": "test",
+        "streetAddress": "test",
+        "city": "test",
+        "state": "test",
+        "country": "test",
+        "pincode": "test",
+        "ownerName": "test",
+        "ownerDOB": "test",
+        "ownerSex": "test",
+        "dogName": "test",
+        "dogBreed": "test",
+        "dogDOB": "test",
+        "dogSex": "test",
+        "dogsFavoriteActivities": "test",
+    }
     database.insertUser(exampleDict)
 
-createExampleNames()    
+
+createExampleNames()
+
+
 @app.route("/feed/<username>", methods=["GET", "OPTIONS"])
 def returnPeopleAtLocation(username):
     if request.method == "GET":
@@ -50,13 +54,41 @@ def register():
         return res
     if request.method == "POST":
         requestedData = json.loads(request.data)
-        inputUsername = requestedData["username"]
-        inputPassword = requestedData["password"]
-        inputConfirm = requestedData["confirm"]
-        database.insertUser(requestedData)
-        return Response(json.dumps({"error": "SUCCESS"}), status=200)
+        inputUsername = str(requestedData["username"]).strip()
+        inputPassword = str(requestedData["password"]).strip()
+        inputConfirm = str(requestedData["confirm"]).strip()
+        if inputUsername == "" or inputPassword == "" or inputConfirm == "":
+            return Response(json.dumps({"error": "Invalid Inputs"}), status=202)
+        elif len(inputUsername) < 4 or len(inputUsername) > 16:
+            return Response(
+                json.dumps({"error": "Username must be between 4 to 16 characters"}),
+                status=202,
+            )
+        elif len(inputPassword) < 8 or len(inputPassword) > 16:
+            return Response(
+                json.dumps({"error": "Password must be between 8 to 16 characters"}),
+                status=202,
+            )
+        elif (not re.compile(r"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).*$").match(inputPassword)):
+            return Response(
+                json.dumps({"error": "Username must be contain atleast 1 alphabet, 1 digit and 1 symbol"}),
+                status=202,
+            )
+        elif (inputPassword!=inputConfirm):
+            return Response(
+                json.dumps({"error": "Please ensure Confirm Password matches Password"}),
+                status=202,
+            )
         
-    return Response(json.dumps({"error": "Incorrect username or password"}), status=201)
+
+        elif database.getUser(inputUsername, None) != None:
+            return Response(json.dumps({"error": "User already exists"}), status=203)
+        else:
+            print(database.getUser(inputUsername, None))
+            database.insertUser(requestedData)
+            return Response(json.dumps({"error": "SUCCESS"}), status=200)
+
+    return Response(json.dumps({"error": "Neither Post nor Option"}), status=399)
 
 
 @app.route("/login", methods=["POST", "OPTIONS"])
@@ -69,12 +101,14 @@ def login():
 
     if request.method == "POST":
         requestedData = json.loads(request.data)
-        inputUsername = requestedData["username"]
-        inputPassword = requestedData["password"]
-        if inputPassword != "" and inputPassword != None:
-            matchedRow = placeHolderSql.returnMatchedRow(inputUsername, inputPassword)
-            if matchedRow != None:
-                return jsonify(matchedRow)
+        inputUsername = requestedData["username"].strip()
+        inputPassword = requestedData["password"].strip()
+        if inputUsername == "" or inputPassword == "":
+            return Response(json.dumps({"error": "Invalid Inputs"}), status=202)
+        else:
+            matchedRow = database.getUser(inputUsername, inputPassword)
+        if matchedRow != None:
+            return Response(status=200)
     return Response(json.dumps({"error": "Incorrect username or password"}), status=201)
 
 
@@ -82,8 +116,8 @@ def login():
 def returnUsername(name):
     # getname
     # print(request.args.get("n"))
-    print(str(database.getUser(name,None)))
-    return database.getUser(name,None)
+    print(str(database.getUser(name, None)))
+    return database.getUser(name, None)
 
 
 if __name__ == "__main__":
