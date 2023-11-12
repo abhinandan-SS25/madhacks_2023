061d38a7cfc124b7fca445d670f5976bfe49fa41
arnav0red <arnav.mohakud@gmail.com>
2023-11-12 04:48:41 -0600

    added placeholder popular trails

--- reactConnector.py
+++ reactConnector.py
@@ -26,6 +26,42 @@ dataFormat = {
     "dogsFavoriteActivities": "test",
 }
 
+placeHolderTrail = [
+    {
+        "trail": {
+            "coordinates": [
+                [88.40329706668854, 22.49435178996269],
+                [88.40484738349915, 22.494153713312638],
+            ],
+            "type": "Polygon",
+        },
+        "likes": 0,
+        "onTrail": 0,
+    },
+    {
+        "trail": {
+            "coordinates": [
+                [88.40118885040285, 22.49557405191004],
+                [88.41170310974123, 22.49636634867535],
+            ],
+            "type": "Polygon",
+        },
+        "likes": 2,
+        "onTrail": 1,
+    },
+    {
+        "trail": {
+            "coordinates": [
+                [88.46019744873048, 22.49630063972289],
+                [88.41522216796876, 22.470627905020283],
+            ],
+            "type": "Polygon",
+        },
+        "likes": 3,
+        "onTrail": 2,
+    },
+]
+
 
 def createExampleNames():
     exampleDict1 = {
@@ -109,7 +145,7 @@ def returnPeopleAtLocation(username):
         tempVal = database.usersNearby(username)
         val["data"] = tempVal
         val["status"] = 200
-
+        val["trails"] = placeHolderTrail
         return Response(json.dumps(val), status=200)
 
 
